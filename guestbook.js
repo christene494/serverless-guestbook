7d80e4259fbb63d90a8260464fa93a580eaa3981
christene494 <84256327+christene494@users.noreply.github.com>
2021-12-30 17:35:15 -0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e17deb21.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
